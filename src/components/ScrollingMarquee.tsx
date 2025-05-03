653be7e7f0bd673abc6bf27dab53c6fd1b788dbc
Jacob Aicher <jake@anoleimaging.com>
2025-05-02 22:42:46 -0400

    feat: implement useDocumentSize and useElementWidth in ScrollingMarquee (initial test integration)

--- src/components/ScrollingMarquee.tsx
+++ src/components/ScrollingMarquee.tsx
@@ -2,31 +2,33 @@
 
 import React from "react";
 import { useState, useRef, useEffect} from "react";
+import useDocumentSize from "@/hooks/useDocumentSize"
+import { useElementWidth } from "@/hooks/useElementWidth";
 
 
 
 
 const ScrollingMarquee = ({children}: {children: React.ReactNode}) =>{
     const contentWidthRef = useRef<HTMLDivElement> (null);
-
+    const {width, height} = useDocumentSize();
+    const {ref: boxRef, width: boxWidth} = useElementWidth<HTMLDivElement>();
+    
     useEffect(() => {
         if(contentWidthRef.current != null)
             console.log(getWidthOfElement(contentWidthRef.current));
-
-
-
     }, []);
+
+    console.log("box width: ",Math.round(boxWidth))
+    console.log("total width: ", width)
+    console.log('')
 
     const getWidthOfElement = (Widthelement:HTMLDivElement) => {
         return Widthelement.getBoundingClientRect().width
     } 
 
-    
-
-
 return(
     <>
-    <div className="w-full h-fit overflow-x-hidden">
+    <div className="w-full h-fit overflow-x-hidden" ref={boxRef}>
         <div className="flex flex-row text-nowrap"
         style={{
             transform: `translateX(${0}px)`,
