aca202bd0834fb1371d0ebb70292f124d257f454
Jacob Aicher <jake@anoleimaging.com>
2025-05-11 00:07:02 -0400

    feat: add initial and on hover rotation

--- src/components/CardImage.tsx
+++ src/components/CardImage.tsx
@@ -8,6 +8,8 @@ interface CardImageProps {
     imgScalePercentage?: {initial:number, zoom: number}, // the percentage amount the image will zoom on cell hover
     imgTranslate?: {x:string, y: string}, // the translation of the image to center subject. used together with img initial scale to prevent edges from showing
     imgTranslateOnHover?: {x:string, y: string},
+    rotation?: number; //sets the initial rotation of the image in degrees. defaults to 0
+    rotationOnHover?: number; //sets the rotation in degrees on hover. defaults to 0
 }
 
 
@@ -16,6 +18,8 @@ export const CardImage = ({
     imgScalePercentage={initial:100, zoom: 100},
     imgTranslate={x:'0',y:'0'},
     imgTranslateOnHover={x:'0',y:'0'},
+    rotation=0,
+    rotationOnHover=0,
 
 }: CardImageProps) => {
     const hovered = useHover();
@@ -27,7 +31,7 @@ export const CardImage = ({
         <img className={`w-full h-full object-cover overflow-visible transition-transform`} 
         src={`${src}`} 
         style={{
-            transform: `scale(${hovered ? imgScalePercentage.zoom : imgScalePercentage.initial}%) translate(${hovered ? imgTranslateOnHover.x : imgTranslate.x}px, ${hovered ? imgTranslateOnHover.y : imgTranslate.y}px)`,
+            transform: `scale(${hovered ? imgScalePercentage.zoom : imgScalePercentage.initial}%) translate(${hovered ? imgTranslateOnHover.x : imgTranslate.x}px, ${hovered ? imgTranslateOnHover.y : imgTranslate.y}px) rotate(${hovered ?rotationOnHover : rotation}deg)`, 
         }}
         /></div>
         </>
