af2ab19fac4dcaf5afb2f9186d83e21878dc4d48
Jacob Aicher <jake@anoleimaging.com>
2025-05-10 23:51:40 -0400

    feat: add `imgTranslateOnHover` prop to control translation transition on hover

--- src/components/CardImage.tsx
+++ src/components/CardImage.tsx
@@ -7,7 +7,7 @@ interface CardImageProps {
     hovered?:Boolean;
     imgScalePercentage?: {initial:number, zoom: number}, // the percentage amount the image will zoom on cell hover
     imgTranslate?: {x:string, y: string}, // the translation of the image to center subject. used together with img initial scale to prevent edges from showing
-
+    imgTranslateOnHover?: {x:string, y: string},
 }
 
 
@@ -15,7 +15,7 @@ export const CardImage = ({
     src, 
     imgScalePercentage={initial:100, zoom: 100},
     imgTranslate={x:'0',y:'0'},
-    
+    imgTranslateOnHover={x:'0',y:'0'},
 
 }: CardImageProps) => {
     const hovered = useHover();
@@ -27,7 +27,7 @@ export const CardImage = ({
         <img className={`w-full h-full object-cover overflow-visible transition-transform`} 
         src={`${src}`} 
         style={{
-            transform: `scale(${hovered ? imgScalePercentage.zoom : imgScalePercentage.initial}%) translate(${imgTranslate.x}px, ${imgTranslate.y}px)`,
+            transform: `scale(${hovered ? imgScalePercentage.zoom : imgScalePercentage.initial}%) translate(${hovered ? imgTranslateOnHover.x : imgTranslate.x}px, ${hovered ? imgTranslateOnHover.y : imgTranslate.y}px)`,
         }}
         /></div>
         </>
