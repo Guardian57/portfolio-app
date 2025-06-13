867f3c333c560a7da8349d74c370ef43c292f2c9
Jacob Aicher <jake@anoleimaging.com>
2025-06-12 23:38:50 -0400

    feat: add Postcard element for contact card overlay

---
+++ src/components/Postcard.tsx
@@ -0,0 +1,18 @@
+
+
+
+const Postcard = () => {
+
+
+
+    return (
+        <div className="fixed w-full h-full bg-logo-blue opacity-90 z-50">
+
+
+        </div>
+
+
+    )
+}
+
+export default Postcard
