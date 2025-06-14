f9c250636e200b3daf5c8f5b061517791f3b5433
Jacob Aicher <jake@anoleimaging.com>
2025-06-14 14:13:18 -0400

    feat:blockout basic hover interaction of postcard contact form

--- src/components/Postcard.tsx
+++ src/components/Postcard.tsx
@@ -2,14 +2,26 @@
 
 
 const Postcard = () => {
-
+    let overlay = true; //temporarily disables overlay for WIP
 
 
     return (
-        <div className="fixed w-full h-full bg-logo-blue opacity-90 z-50">
+        <>
+            {/* BACKGROUND OVERLAY - for when the contact form is expanded */}
+            <div className={`fixed w-full h-full bottom-4 bg-logo-blue opacity-90 z-50 ${overlay ? "hidden" : ""}`}>
+            </div>
 
+            {/* TRIGGER ZONE - for triggering the expansion and hover effects */}
 
-        </div>
+            <div className={"fixed w-full h-full bg-teal-400 -bottom-[90vh] z-50 group flex content-center justify-center"}>
+                <div className={"w-1/2 h-auto bottom-0 bg-yellow-100  group-hover:-translate-y-28 transition"}>
+
+                </div>
+
+            </div>
+
+        </>
+        
 
 
     )
