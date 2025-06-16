bdba7b1bd5e0229ad4556525bd56154c4c8c8dad
Jacob Aicher <jake@anoleimaging.com>
2025-06-15 23:12:12 -0400

    refactor: add function to organize dynamic tailwind classes and improve css transitions

--- src/components/Postcard.tsx
+++ src/components/Postcard.tsx
@@ -3,7 +3,10 @@ import { useScrollPercent } from "@/hooks/useScrollPercent";
 import exp from "constants";
 import { useEffect, useState } from "react";
 
-
+interface triggerClassResults{
+    trigger: string,
+    card: string,
+}
 
 const Postcard = () => {
     let b = useScrollPercent().scrollPercentage
@@ -11,7 +14,7 @@ const Postcard = () => {
     const [expanded, setExpanded] = useState<boolean>(false);
     const [scrollHalf, setScrollHalf] = useState<boolean>(false);
     const [scrollFull, setScrollFull] = useState<boolean>(false);
-
+    const [classes, setClasses] = useState<triggerClassResults>({trigger: '', card: ''});
 
 
     useEffect(() => {
@@ -30,24 +33,58 @@ const Postcard = () => {
         
     }, [b])
 
+    useEffect(() => {
+
+        setClasses(triggerClass(expanded,b))
+
+    }, [expanded, scrollHalf, scrollFull])
+
     const pointDown = () => {
         setExpanded(true)
+    }
+
+    const triggerClass = (expanded: boolean, b: number): triggerClassResults => {
+        let triggerClasses = ''
+        let CardClasses = ''
+        
+        if (expanded){
+            triggerClasses = 'bottom-0 bg-opacity-90'
+            CardClasses = 'top-1/2 -translate-y-1/2'
+        } else {
+            if(b < 50){
+                triggerClasses = '-bottom-[101vh] bg-opacity-0'
+                CardClasses = 'top-0'
+            } else if (b < 90){
+                triggerClasses = '-bottom-[90vh] bg-opacity-0'
+                CardClasses = 'top-1/2 group-hover:top-0'
+            } else {
+                triggerClasses = '-bottom-[90vh] bg-opacity-0'
+                CardClasses = 'top-0 group-hover:-top-10'
+            } 
+        }
+
+        return {
+            trigger: triggerClasses,
+            card: CardClasses,
+        }
+
     }
 
     return (
         <>
             {/* BACKGROUND OVERLAY - for when the contact form is expanded */}
-            <div className={`fixed w-full h-full bg-logo-blue z-50 ${expanded ? "block bg-opacity-90" : "hidden bg-opacity-0"} transition-all`}
+            {/* <div className={`fixed w-full h-full bg-logo-blue z-50 ${expanded ? "block bg-opacity-90" : "hidden bg-opacity-0"} transition-all`}
             
             >
-            </div>
+            </div> */}
 
             {/* TRIGGER ZONE - for triggering the expansion and hover effects */}
 
-            <div className={`fixed w-full h-full flex flex-wrap bg-teal-200 ${ expanded ? 'bottom-0 content-center': scrollHalf ? '-bottom-[90vh]' : '-bottom-[101vh]'} z-50 group  justify-center transition-all duration-500`}
+            <div className={`fixed w-full h-full flex flex-wrap bg-logo-blue translat ${classes.trigger} z-50 group justify-center transition-all duration-500`}
+           
             onPointerDown={() => {expanded ? setExpanded(false) : null}}
             >
-                <div className={`relative w-1/2 h-1/2 bg-yellow-100 z-50 ${ expanded ? '' : scrollFull ? 'top-0 group-hover:-translate-y-10' : 'top-[50vh]'} group-hover:top-0 transition-all duration-500`}
+                <div className={`absolute w-1/2 h-1/2 bg-yellow-100 z-50 ${classes.card} transition-all duration-500`}
                 onPointerDown={pointDown}
                 >
                 </div>
